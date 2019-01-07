9e643ecdf70c49d9deded4120b52c73490623d54
stretyakova <stretyakova@PC10140382.cc-college.gecs.loc>
2019-01-07 09:36:52 +0100

    179,180

---
+++ OCP_PDF_Fragen/src/fragen_July_2018/_179.java
@@ -0,0 +1,55 @@
+package fragen_July_2018;
+
+
+public class _179 {
+	public static void main(String[] args) {
+		int array[] = { 10, 20, 30, 40, 50 };
+		int x = array.length;
+		
+		/*line n1*/
+		while (x > 0) {
+	         x--;
+	         System.out.print(array[x]); 
+	        }
+
+	}
+}
+
+/*
+  Which two code fragments can be independently inserted at line n1 
+to enable the code to print the elements of the array in reverse order? 
+*/
+
+
+
+
+/*
+ A) while(x>0) {
+ 
+       System.out.print(array[--x]);
+}   
+
+B) do {
+      x--;
+      System.out.print(array[x]);
+      }while(x>=0); 
+
+C) do {
+     System.out.print(array[x]);
+      --x;
+      } while (x>=0);
+      
+D) while (x>=0){
+       System.out.print(array[x]);
+       x--;
+       }
+ 
+E)while (x > 0) {
+         x--;
+         System.out.print(array[x]); 
+        }
+*/
+
+
+
+// A and E sind richtig
