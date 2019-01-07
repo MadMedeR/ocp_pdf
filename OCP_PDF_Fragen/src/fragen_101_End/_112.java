44d88a37e7e29554ab2a05ef5ddc2174ca515ed9
Sebastian <sbydalek@PC10150116.cc-college.gecs.loc>
2019-01-07 12:02:20 +0100

    bEENDET

--- OCP_PDF_Fragen/src/fragen_101_End/_112.java
+++ OCP_PDF_Fragen/src/fragen_101_End/_112.java
@@ -13,10 +13,14 @@ public static void main(String[] args) {
 	
 // Moveable<Integer> animal = n -> n + 10; animal.run(100); animal.walk(20);
 	
-	Moveable animal = (Integer n) -> System.out.println(n); animal.run(100); Moveable.walk(20);	
+//	Moveable animal = (Integer n) -> System.out.println(n); 
+//	animal.run(100);
+//	Moveable.walk(20);	
+	
+	
+	
 }
 }
-
 
 // Answer A:  	Running100
 // 				Walking
@@ -24,4 +28,11 @@ public static void main(String[] args) {
 //Answer B:   Exception in thread "main" java.lang.Error: Unresolved compilation problem: 
 // Void methods cannot return a value
 
-// Andwer C: 
+// Answer C: Exception in thread "main" java.lang.Error: Unresolved compilation problems: 
+//Lambda expression's parameter n is expected to be of type Object
+// Moveable ist generisch Interface wann Moveable dann es ist Moveable<Object> und 
+// Lambda soll Moveable animal = (Object  n) -> System.out.println(n); 
+// oder Moveable<?> animal = (Integer n) -> System.out.println(n);  sehen
+
+
+// D ist falsch weil A ist richtig :).
