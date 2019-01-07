13612a1693a4f3f34bc96656217a50c1eaad71a8
Sebastian <sbydalek@PC10150116.cc-college.gecs.loc>
2019-01-07 11:35:29 +0100

    Seb

---
+++ OCP_PDF_Fragen/src/fragen_101_End/_108.java
@@ -0,0 +1,18 @@
+package fragen_101_End;
+
+public class _108 {
+	/*
+	 * Which two statements are true for a two-dimensional array?
+A. It is implemented as an array of the specified element type.
+B.	Using a row by column convention,
+  	each row of a two-dimensional array must be of the same size.
+C. 	At declaration time, 
+ 	the number of elements of the array in each dimension must be specified. 
+D. 	All methods of the class Object may be invoked on the two-dimensional array.
+	 */
+	
+	
+// 	AD    	int [][] a = new int [4][];
+	
+
+}
---
+++ OCP_PDF_Fragen/src/fragen_101_End/_109.java
@@ -0,0 +1,24 @@
+package fragen_101_End;
+
+/**
+ * Which action encapsulates the Vehicle class?
+	A. Make the Vehicle class public. 
+	B. Make the name variable public. 
+	C. Make the setName method public. 
+	D. Make the name variable private. 
+	E. Make the setName method private. 
+	F. Make the getName method private.
+ */
+public class _109 {}
+
+class Vehicle {
+String name;
+void setName (String name) {
+this.name = name;
+}
+String getName() {
+return name;
+}
+}
+
+// D
