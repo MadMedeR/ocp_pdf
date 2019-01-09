286c4cb3b9763e58ff67fab8342ab955c6d8bc59
stretyakova <stretyakova@PC10140382.cc-college.gecs.loc>
2019-01-09 09:01:38 +0100

    Neu Svetlana

---
+++ OCP_PDF_Fragen/src/fragen_1_20/_08.java
@@ -0,0 +1,41 @@
+package fragen_1_20;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class _08 {
+	public static void main(String[] args) {
+		List<TechName> tech = Arrays.asList(new TechName("Java-"), 
+				new TechName("Oracle DB-"),
+				new TechName("J2EE-"));
+		Stream<TechName> stre = tech.stream();
+		// stre.forEach(System.out::print);
+		stre.map(a -> a.techName).forEach(System.out::print);
+		// stre.map(a-> a).forEachOrdered(System.out::print);
+		// stre.forEachOrdered(System.out::print);
+		
+	}
+}
+
+class TechName {
+	String techName;
+
+	TechName(String techName) {
+		this.techName = techName;
+	}
+}
+
+/*
+Which should be inserted at line n1 to print Java-Oracle DB-J2EE-? 
+
+A.  stre.forEach(System.out::print); 
+B.  stre.map(a-> a.techName).forEach(System.out::print); 
+C.  stre.map(a-> a).forEachOrdered(System.out::print); 
+D.  stre.forEachOrdered(System.out::print); 
+*/
+
+//https://www.briefmenow.org/oracle/which-should-be-inserted-at-line-n1-to-print-java-oracl/
+// Frage 10
+
+//Answer B   stre.map(a -> a.techName).forEach(System.out::print);
---
+++ OCP_PDF_Fragen/src/fragen_1_20/_14.java
@@ -0,0 +1,47 @@
+package fragen_1_20;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class _14 {
+public static void main(String[] args) {
+	List<Emp> emp = Arrays.asList ( 
+			new Emp ("John", "Smith"), 
+			new Emp ("Peter", "Sam"), 
+			new Emp ("Thomas", "Wale")); 
+			emp.stream()
+			.sorted (Comparator.comparing(Emp::getfName).reserved()/*reversed()*/.thenComparing(Emp::getlName))
+			//line n1 
+			.collect(Collectors.toList()); 
+}
+}
+
+ class Emp { 
+String fName; 
+String lName; 
+public Emp (String fn, String ln) { 
+fName = fn; 
+lName = ln; 
+} 
+public String getfName() { return fName; } 
+public String getlName() { return lName; } 
+} 
+ 
+ 
+ /*
+   Which code fragment, when inserted at line n1, sorts the employees list in descending order of 
+fName and then ascending order of lName? 
+ 
+A.  .sorted(Comparator.comparing(Emp::getfName).reserved().thenComparing(Emp::getlName
+)) 
+B.  .sorted(Comparator.comparing(Emp::getfName).thenComparing(Emp::getlName)) 
+C.  .map(Emp::getfName).sorted(Comparator.reserveOrder()) 
+D.  .map(Emp::getfName).sorted(Comparator.reserveOrder().map(Emp::getlName).reserved 
+  */
+ 
+ 
+ // https://www.briefmenow.org/oracle/which-code-fragment-when-inserted-at-line-n1-sorts-th/
+ //Frage 60
+ // Answer A (see comments)
