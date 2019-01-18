3710a94fbd64047c12119589bf793443eecf13ac
Sebastian <sbydalek@PC10150116.cc-college.gecs.loc>
2019-01-18 08:57:06 +0100

    107 Korigiert

--- OCP_PDF_Fragen/src/fragen_101_End/_107.java
+++ OCP_PDF_Fragen/src/fragen_101_End/_107.java
@@ -12,14 +12,14 @@ public static void main(String[] args) {
 	
 //	this.fvar = 200; this.cvar = 400;
 	
-//	t.fvar = 200; Test2.cvar = 400;
+	t.fvar = 200; _107.cvar = 400;
 	
 //	this.fvar = 200; Test2.cvar = 400;	
 }
 }
 
 
-// A, B Compiles
+// A, B E Compiles
 
 // C Exception in thread "main" java.lang.Error: Unresolved compilation problem: 
 // Cannot make a static reference to the non-static field fvar
@@ -28,8 +28,7 @@ public static void main(String[] args) {
 // Cannot use this in a static context
 // Cannot use this in a static context
 
-// E Exception in thread "main" java.lang.Error: Unresolved compilation problem: 
-// Test2 cannot be resolved to a variable
+// E
 
 //F Exception in thread "main" java.lang.Error: Unresolved compilation problems: 
 //Cannot use this in a static context
