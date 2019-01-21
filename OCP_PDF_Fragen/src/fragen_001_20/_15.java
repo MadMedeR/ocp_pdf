fb2a4b1b18b19dc826eefc0d3484acd2538af5f6
MadMedeR <46371250+MadMedeR@users.noreply.github.com>
2019-01-21 16:30:39 +0100

    Explanation added

--- OCP_PDF_Fragen/src/fragen_001_20/_15.java
+++ OCP_PDF_Fragen/src/fragen_001_20/_15.java
@@ -1,21 +1,21 @@
 package fragen_001_20;
 
+@SuppressWarnings("unused")
 public class _15 {
 	public static void main(String[] args) {
-		//A. Car auto = Car(“MyCar”)::new;
-		//B. Car auto = Car::new; Car vehicle = auto::getCar(“MyCar”);
-		//C. (Only one correct):
-		Rideable rider = Car::new; 
+		//Solved with Answer C: (Only one correct):
+		Rideable rider = Car::new; // same as: Rideable rider = (s) -> new Car(s);
 		Car vehicle = rider.getCar("MyCar");
-		//D. Car vehicle = Rideable::new::getCar(“MyCar”);
+		
 		System.out.println(rider.toString());
-		}
+	}
 }
 
 interface Rideable {
 	Car getCar(String name);
 }
 
+@SuppressWarnings("unused")
 class Car {
 	private String name;
 
@@ -25,18 +25,22 @@ class Car {
 }
 
 /*
-Which code fragment creates an instance of Car? 
+	Which code fragment creates an instance of Car? 
 
-A.  Car auto = Car ("MyCar"): : new; 
-B.  Car auto = Car : : new; 
-Car vehicle = auto : : getCar("MyCar"); 
-C.  Rideable rider = Car : : new; 
-Car vehicle = rider.getCar("MyCar"); 
-D.  Car vehicle = Rideable : : new : : getCar("MyCar"); 
+	A.  Car auto = Car ("MyCar"): : new; 
+	B.  Car auto = Car : : new; 
+		Car vehicle = auto : : getCar("MyCar"); 
+	C.  Rideable rider = Car : : new; 
+		Car vehicle = rider.getCar("MyCar"); 
+	D.  Car vehicle = Rideable : : new : : getCar("MyCar"); 
 
+	Answer: only C
+
+	https://www.briefmenow.org/oracle/which-code-fragment-creates-an-instance-of-car/
+	siehe auch Frage 43
+	
+	Explanation:
+	The method of the Instance Rideable: Car getCar(String name)
+	and the Constructor of the class Car: new Car(String name) are doing the same.
+	That's why you can use this Constructor as a function of the lambda expression.
 */
-
-//https://www.briefmenow.org/oracle/which-code-fragment-creates-an-instance-of-car/
-//Frage 43
-
-//Answer C
