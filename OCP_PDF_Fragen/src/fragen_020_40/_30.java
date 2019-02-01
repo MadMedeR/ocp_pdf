846da7aa2bdd742852e5d4e42444a2b893456ff2
MadMedeR <46371250+MadMedeR@users.noreply.github.com>
2019-02-01 15:13:17 +0100

    Update

--- OCP_PDF_Fragen/src/fragen_020_40/_30.java
+++ OCP_PDF_Fragen/src/fragen_020_40/_30.java
@@ -1,18 +1,16 @@
 package fragen_020_40;
 
-public class _30 {
-//TODO databases
-	
-/**	
+public class _30 {}
+/*
 	Which action can be used to load a database driver by using JDBC3.0?
-			A. Add the driver class to the META-INF/services folder of the JAR file.
-			B. Include the JDBC driver class in a jdbc.properties file.
-				C. Use the java.lang.Class.forName method to load the driver class.
-			D. Use the DriverManager.getDriver method to load the driver class.
-			
-			
-			
-			from Internet not sure if correct Answer C
-			
-**/			
-}
+
+	A. Add the driver class to the META-INF/services folder of the JAR file.
+	B. Include the JDBC driver class in a jdbc.properties file.
+	C. Use the java.lang.Class.forName method to load the driver class.
+	D. Use the DriverManager.getDriver method to load the driver class.
+
+	Answer: C
+	
+	siehe auch web_67
+	http://www.briefmenow.org/oracle/which-action-can-be-used-to-load-a-database-driver-by-u/
+ */
