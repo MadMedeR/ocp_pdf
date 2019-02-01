8cc4090a7ab16d6757111a5ad3ba8106b546a261
MadMedeR <46371250+MadMedeR@users.noreply.github.com>
2019-02-01 14:48:43 +0100

    Update

--- OCP_PDF_Fragen/src/fragen_020_40/_29.java
+++ OCP_PDF_Fragen/src/fragen_020_40/_29.java
@@ -1,5 +1,67 @@
 package fragen_020_40;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/*
+	Given the records from the Employee table:
+	+-----+---------+
+	| eid | ename	|
+	+-----+---------+
+	| 111 | Tom		|
+	| 112 | Jerry	|
+	| 113 | Donald	|
+	+-----+---------+	
+ */
+
 public class _29 {
-// TODO databases
+	private static String URL, userName, passWord;
+
+	public static void main(String[] args) {
+		try	{
+			Connection conn = DriverManager.getConnection(URL,userName,passWord);
+			Statement st = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_UPDATABLE);
+			st.execute("SELECT*FROM Employee");
+			ResultSet rs = st.getResultSet();
+		while (rs.next()) {
+			if (rs.getInt(1)==112) {
+				rs.updateString(2,"Jack");
+			}
+		}
+		
+		rs.absolute(2);
+		System.out.println(rs.getInt(1) + " " + rs.getString(2));
+		} catch(SQLException ex) {
+			System.out.println("Exception is raised");
+		}
+	}
 }
+
+/*
+	Assume that:
+	- The required database driver is configured in the classpath.
+	- The appropriate database accessible with the URL, userName, and passWord exists.
+	
+	What is the result?
+	
+	A.	The Employee table is updated with the row:
+		112 Jack
+		and the program prints:
+		112 Jerry
+	B.	The Employee table is updated with the row:
+		112 Jack
+		and the program prints:
+		112 Jack
+	C.	The Employee table is not updated and the program prints:
+		112 Jerry
+	D.	The program prints:
+		Exception is raised
+	
+	Answer: C
+	
+	siehe auch web_15
+	http://www.briefmenow.org/oracle/what-is-the-result-966/
+*/
