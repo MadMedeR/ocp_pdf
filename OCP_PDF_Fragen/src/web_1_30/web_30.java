a89492fb6f4f7d2855a3e4227158b915bfe01270
MadMedeR <46371250+MadMedeR@users.noreply.github.com>
2019-02-01 15:02:30 +0100

    Update

--- OCP_PDF_Fragen/src/web_1_30/web_30.java
+++ OCP_PDF_Fragen/src/web_1_30/web_30.java
@@ -1,36 +1,56 @@
 package web_1_30;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/*
+	Given:
+	Item table
+	• ID, INTEGER: PK
+	• DESCRIPTION, VARCHAR(100)
+	• PRICE, REAL
+	• QUANTITY, INTEGER
+*/
 
 public class web_30 {
-     
+	private static String dbURL, username, password;
+
+	public static void main(String[] args) {
+		try {
+			Connection conn = DriverManager.getConnection(dbURL, username, password);
+			String query = "Select * FROM Item WHERE ID = 110";
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			while(rs.next()) {
+				System.out.println("ID: " + rs.getInt("Id"));
+				System.out.println("Description: " + rs.getString("Descrip"));
+				System.out.println("Price: " + rs.getDouble("Price"));
+				System.out.println("Quantity: " + rs.getInt("Quantity"));
+			}
+		} catch (SQLException se) {
+			System.out.println("Error");
+		}
+	}
 }
 
-
-/*Given:
-Item table
-• ID, INTEGER: PK
-• DESCRIP, VARCHAR(100)
-• PRICE, REAL
-• QUANTITY< INTEGER
-And given the code fragment:
-9. try {
-10. Connection conn = DriveManager.getConnection(dbURL, username, password);
-11. String query = “Select * FROM Item WHERE ID = 110”;
-12. Statement stmt = conn.createStatement();
-13. ResultSet rs = stmt.executeQuery(query);
-14. while(rs.next()) {
-15. System.out.println(“ID: “ + rs.getInt(“Id”));
-16. System.out.println(“Description: “ + rs.getString(“Descrip”));
-17. System.out.println(“Price: “ + rs.getDouble(“Price”));
-18. System.out.println(Quantity: “ + rs.getInt(“Quantity”));
-19. }
-20. } catch (SQLException se) {
-21. System.out.println(“Error”);
-22. }
-Assume that:
-The required database driver is configured in the classpath.
-The appropriate database is accessible with the dbURL, userName, and passWord exists.
-The SQL query is valid.
-What is the result?*/
-
-//Answer?
+/*
+	Assume that:
+	The required database driver is configured in the classpath.
+	The appropriate database is accessible with the dbURL, userName, and passWord exists.
+	The SQL query is valid.
+	
+	What is the result?
+	
+	A. An exception is thrown at runtime.
+	B. Compilation fails.
+	C. The code prints Error.
+	D. The code prints information about Item 110.
+	
+	Answer: D
+	
+	http://www.briefmenow.org/oracle/what-is-the-result-958/
+	
+*/
