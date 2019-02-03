39f9a130ea2de6edbf30fc662f65acbb302dc283
MadMedeR <46371250+MadMedeR@users.noreply.github.com>
2019-02-03 22:35:52 +0100

    Update

--- OCP_PDF_Fragen/src/fragen_081_100/_093.java
+++ OCP_PDF_Fragen/src/fragen_081_100/_093.java
@@ -8,10 +8,11 @@ import java.util.stream.Stream;
 public class _093 {
 	public static void main(String[] args) {
 		Stream<List<String>> iStr= Stream.of(
-				Arrays.asList ("1", "John"), 
-				Arrays.asList ("2", null)); 
-		Stream<String> nInSt = iStr.flatMapToInt((x) -> x.stream()); 
-		nInSt.forEach (System.out :: print); 
+				Arrays.asList ("1", "John"),
+				Arrays.asList ("2", null));
+//		Stream<String> nInSt = iStr.flatMapToInt((x) -> x.stream());	//original line
+		Stream<String> nInSt = iStr.flatMap((x) -> x.stream()); 
+		nInSt.forEach (System.out :: print);
 	}
 }
 
@@ -24,10 +25,9 @@ public class _093 {
 	D. A compilation error occurs
 
 	Answer: D
-	Exception in thread "main" java.lang.Error: Unresolved compilation problems: 
-	Type mismatch: cannot convert from IntStream to Stream<String>
-	Type mismatch: cannot convert from Stream<String> to IntStream
-
-	at fragen_081_100._93.main(_93.java:13)
 	
+	If there would be
+	Stream<String> nInSt = iStr.flatMap((x) -> x.stream()); oder
+	Stream<String> nInSt = iStr.flatMap(List::stream); 
+	Answer would be: A
 */
